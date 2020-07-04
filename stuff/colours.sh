f159a9512689440d5fcd4a26672659bfa2eda5d7
Xan Manning <xmanning@centiq.co.uk>
2020-07-04 13:05:34 +0000

    Updated slideshow to be more fun

---
+++ stuff/colours.sh
@@ -0,0 +1,6 @@
+readonly red="$(tput setaf 1)$(tput bold)"
+readonly green="$(tput setaf 2)$(tput bold)"
+readonly blue="$(tput setaf 6)$(tput bold)"
+readonly purple="$(tput setaf 5)$(tput bold)"
+readonly reset="$(tput sgr0)"
+
